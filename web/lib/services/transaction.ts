8865dfbfbe6ab44ea1e84a98384dfdd1a6df800e
r33anz <anzaldoalvaradorodrigo@gmail.com>
2025-08-23 23:28:04 -0600

    tx

--- web/lib/services/transaction.ts
+++ web/lib/services/transaction.ts
@@ -1,10 +1,26 @@
-// Transaction Service - centralized transaction logic
-export class TransactionService {
-  static async createTransaction(senderPhone: string, receiverPhone: string, amount: string) {
-    // Implementation will be added here
-    throw new Error('Not implemented')
+import {parseEther,isAddress,parseGwei} from 'viem'
+import { Para, Environment, WalletType } from "@getpara/server-sdk";
+import WalletService from './wallet'
+import { IWalletRepository } from '../interfaces/IWalletRepository';
+import dotenv from "dotenv";
+import { createParaAccount ,createParaViemClient} from "@getpara/viem-v2-integration";
+//import { createParaAccount, createParaViemClient } from "@getpara/viem";
+dotenv.config();
+
+class TransactionService {
+
+  private walletService: WalletService; 
+  private paraServer : Para;
+
+  constructor(walletRepository: IWalletRepository) {
+    this.walletService = new WalletService(walletRepository);
+    this.paraServer = new Para(process.env.PARA_API_KEY || '');
   }
 
+  async createTransaction(senderPhone: number, receiverPhone: number, amount: string) {
+  
+  }
+    
   static async getTransactionStatus(transactionId: string) {
     // Implementation will be added here
     throw new Error('Not implemented')
@@ -20,3 +36,5 @@ export class TransactionService {
     throw new Error('Not implemented')
   }
 }
+
+export default TransactionService;
